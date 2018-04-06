966f9c4936a7b78fb3b2a73f047d7d370cac7cd6
Sckylle <35964176+Sckylle@users.noreply.github.com>
2018-04-06 17:43:18 +0200

    Add files via upload

--- doc/html/search/all_1.js
+++ doc/html/search/all_1.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['calcul_5f1_5fscore_5fplacement',['calcul_1_score_placement',['../IA_8c.html#a1ca52e208b3abaf80d2489d1485fdd1f',1,'IA.c']]],
-  ['calcul_5f2_5fscore_5fplacement',['calcul_2_score_placement',['../IA_8c.html#a722f456d97ecf0103fddc1ac9f472f57',1,'IA.c']]],
-  ['calcule_5fia',['calcule_ia',['../IA_8c.html#ac1021cb0ca1061023506f23a3bf831d1',1,'IA.c']]],
+  ['calcul_5f1_5fscore_5fplacement',['calcul_1_score_placement',['../_i_a_8c.html#a1ca52e208b3abaf80d2489d1485fdd1f',1,'IA.c']]],
+  ['calcul_5f2_5fscore_5fplacement',['calcul_2_score_placement',['../_i_a_8c.html#a5bf1613f7f947494ccecf8995850fab1',1,'IA.c']]],
+  ['calcule_5fia',['calcule_ia',['../_i_a_8c.html#a8a5af590a4671422aecec34db5282286',1,'IA.c']]],
   ['charger_5fpartie',['charger_partie',['../menu__pause_8c.html#a567460c4b8ba2212718aabec861b1942',1,'menu_pause.c']]],
   ['commencer_5fpartie',['commencer_partie',['../menu__accueil_8c.html#ab10a01dd0e9916eb458ca69959543b9c',1,'menu_accueil.c']]],
   ['connexion_5fserveur',['connexion_serveur',['../reseau_8c.html#ae9e47c397f69d8c57e3fe7fb4b84d152',1,'reseau.c']]],
+  ['coord',['coord',['../structt__retour.html#a41604ec3aac6c0abd76056239bf73f28',1,'t_retour']]],
   ['coordonner_5finvalide',['coordonner_invalide',['../init__plateau_8c.html#a9b93825f8a4a6dd83a6a11aebc045aee',1,'init_plateau.c']]],
   ['copie_5fliste',['copie_liste',['../liste_8c.html#a5cc83f8eeeb903a8b631ef84f9efffcf',1,'liste.c']]],
   ['copie_5fmatrice',['copie_matrice',['../matrice_8c.html#a12313fda94f38f8cf54590e66b4d453f',1,'matrice.c']]],
   ['copie_5fplateau',['copie_plateau',['../init__plateau_8c.html#ac05e76e09561c2c6fdf47c4bab1f7c3a',1,'init_plateau.c']]],
-  ['couleur',['couleur',['../structt__case.html#ae871a70afefeefaeb0003ed141d9d06b',1,'t_case']]]
+  ['couleur',['couleur',['../structt__case.html#a93718ab3d9418a25e7d88c15fcaa4ba2',1,'t_case']]]
 ];
--- doc/html/search/all_2.js
+++ doc/html/search/all_2.js
@@ -3,6 +3,6 @@ var searchData=
   ['en_5fqueue',['en_queue',['../liste_8c.html#a0070ce853ee19eac4f1226988a494e01',1,'liste.c']]],
   ['en_5ftete',['en_tete',['../liste_8c.html#a6f3dc42709b31e9eb527e3018568e766',1,'liste.c']]],
   ['envoyer_5fnumero',['envoyer_numero',['../reseau_8c.html#aea0fee6be243dadc9e9a35d633143994',1,'reseau.c']]],
-  ['envoyer_5fplateau',['envoyer_plateau',['../reseau_8c.html#a068a46b7eba23a7fcccff9ce9480e957',1,'reseau.c']]],
-  ['envoyer_5fplateau_5fclients',['envoyer_plateau_clients',['../reseau_8c.html#a1d1fdf15e373cf2ea1c06c78e0914458',1,'reseau.c']]]
+  ['envoyer_5fplateau',['envoyer_plateau',['../reseau_8c.html#a8ad56034f582019021feed3dee3128bd',1,'reseau.c']]],
+  ['envoyer_5fplateau_5fclients',['envoyer_plateau_clients',['../reseau_8c.html#a41db51cf896f92d120012c45b9e62345',1,'reseau.c']]]
 ];
